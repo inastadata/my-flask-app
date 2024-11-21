b286a67f7f5c7a35de1b403fd9263951a5c30185
inasta <inasta@miyupup.local>
2024-11-21 10:45:15 +0700

    test webhook

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello automated deployment! pakai webook"
+    return "success webhook automation deployment"
 
 if __name__ == "__main__":
     app.run(debug=True)
